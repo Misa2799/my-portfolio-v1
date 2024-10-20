fb360a5e0ec158d2e0b172255a14f5bd213db0f5
Misa Takeda <misatakeda.2799@gmail.com>
2024-10-20 18:42:51 +0900

    initial commit

---
+++ src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuLists} ${menuOpen ? styles.menuOpen : ""}`}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">My Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
